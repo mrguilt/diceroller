a98a5228e042643f14f792800bb6d63ec2010e01
Charles Barilleaux <charles@mrguilt.com>
2025-09-22 23:28:02 -0400

    Reads arguements. Prints a temporary help file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env; //Standard library. To get arguments.
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let num_args = args.len();
+
+    if num_args < 2 {
+        println!("\nERROR! Insufficient arguments!\n");
+        help();
+    }
+
+    let commandline=&args[1].to_lowercase();
+    if commandline == "--help" {
+        help();
+    }
+    println!("First Argument: {commandline}");
+}
+
+fn help() {
+    println!("This is a placeholder for the help text.");
+    process::exit(0);
 }
