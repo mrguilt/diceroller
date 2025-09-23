412b0566dde6698f94edec18f60877a5100093e1
Charles Barilleaux <charles@mrguilt.com>
2025-09-23 00:09:20 -0400

    Got the basic functionality written--I can roll one dice so-many times.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::env; //Standard library. To get arguments.
 use std::process;
+use rand::Rng; // Import the Rng trait
 
 fn main() {
+    let mut rng = rand::thread_rng(); // Get a thread-local random number generator
+
     let args: Vec<String> = env::args().collect();
     let num_args = args.len();
 
@@ -14,10 +17,27 @@ fn main() {
     if commandline == "--help" {
         help();
     }
-    println!("First Argument: {commandline}");
+    let mut rolls: Vec<&str>=commandline.split('d').collect();
+    let mut rollcount:i32=rolls[0].parse().unwrap();
+    let mut sides:i32=rolls[1].parse().unwrap();
+    println!("Rolling a {sides}-sided die {rollcount} times:");
+    let mut rolltotal=0;
+
+    for i in (1..(rollcount+1)) {
+        print!("\tRoll #{i}: ");
+        let result=rng.gen_range(1..(sides+1));
+        printroll(result);
+        rolltotal=rolltotal+result;
+    }
+    println!("Total for {commandline}: {rolltotal}");
 }
 
+fn printroll(result:i32) {
+    println!("{result}");
+}
+
+//This function will be the help text. I'm not going to write anything until I get done.
 fn help() {
     println!("This is a placeholder for the help text.");
-    process::exit(0);
+    process::exit(0);   
 }
